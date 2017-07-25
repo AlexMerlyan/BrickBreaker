280f12afd0ebdef7815f33b81bac8360ddef386a
AlexMerlyan <sasha-merlyan@yandex.com>
2017-07-25 14:44:17 +0300

    Version: 0.0.2 Branch: master Date: 25.07.2017 Author: AlexMerlyan
    Working: bricks
    Changes: creating and deleting every brick when the ball intersects it
    Next: add change direction for ball and modify gameplay
    
    Comment: ))

--- src/com/game/ez/GamePlay.java
+++ src/com/game/ez/GamePlay.java
@@ -25,6 +25,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private int ballDirY;
 
     private Timer timer;
+    private MapGenerator mapGenerator;
 
     public GamePlay() {
         this.totalBricks = 21;
@@ -35,6 +36,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         this.ballPosY = 350;
         this.ballPosX = 120;
         this.ballSpeed = 10;
+        this.mapGenerator = new MapGenerator(3, 7);
         this.timer = new Timer(this.delay, this);
         this.timer.start();
 
@@ -55,6 +57,9 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         g.fillRect(792, 0, 3, 592);
         g.fillRect(0, 570, 792, 3);
 
+        //map
+        mapGenerator.drawMap((Graphics2D) g);
+
         //paddle
         g.setColor(Color.GREEN);
         g.fillRect(playerX, 550, 100, 8);
@@ -63,21 +68,38 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         g.setColor(Color.YELLOW);
         g.fillOval(ballPosX, ballPosY, 20, 20);
 
-       // g.dispose();
+        g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         timer.start();
         if (isPlay) {
+            Rectangle ball = new Rectangle(ballPosX, ballPosY, 20, 20);
+            Rectangle paddle = new Rectangle(playerX, 550, 100, 8);
+            if (ball.intersects(paddle)) {
+                ballDirY = -ballDirY;
+//                ballDirY = ballDirY - 1;
+//                ballDirX = ballDirX - 1;
+            }
+
+            for (int i = 0; i < mapGenerator.getCountOfRows(); i++) {
+                for (int j = 0; j < mapGenerator.getCountOfColumns(); j++) {
+                    if (mapGenerator.getElementOfMap(i, j) > 0) {
+                        int brickX = j * mapGenerator.getBrickWidth() + 80;
+                        int brickY = i * mapGenerator.getBrickHeight() + 50;
+                        Rectangle brick = new Rectangle(brickX, brickY, mapGenerator.getBrickWidth(), mapGenerator.getBrickHeight());
+                        if (ball.intersects(brick)) {
+                            mapGenerator.setBrickValue(0, i, j);
+                            totalBricks--;
+                            score++;
+                        }
+                    }
+                }
+            }
+
             ballPosX = ballPosX + ballDirX;
             ballPosY = ballPosY + ballDirY;
-
-            if (new Rectangle(ballPosX, ballPosY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
-                ballDirY = -ballDirY;
-                ballDirY = ballDirY - 2;
-                ballDirX = ballDirX - 1;
-            }
 
             if (ballPosX >= 800) {
                 ballDirX = -ballDirX;
---
+++ src/com/game/ez/MapGenerator.java
@@ -0,0 +1,62 @@
+package com.game.ez;
+
+import java.awt.*;
+
+/**
+ * Created by Arizel on 25.07.2017.
+ */
+public class MapGenerator {
+    private int map[][];
+    private int brickWidth;
+    private int brickHeight;
+
+    public MapGenerator(int row, int col) {
+        map = new int[row][col];
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = 1;
+            }
+        }
+
+        brickWidth = 650/col;
+        brickHeight = 150/row;
+    }
+
+    public void drawMap(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(Color.black);
+                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                    g.setColor(Color.white);
+                    g.fillRect(j * brickWidth + 82, i * brickHeight + 52, brickWidth - 4, brickHeight - 4);
+                }
+            }
+        }
+    }
+
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
+    }
+
+    public int getElementOfMap(int row, int col) {
+        return map[row][col];
+    }
+
+    public int getCountOfRows() {
+        return map.length;
+    }
+
+    public int getCountOfColumns() {
+        return map[0].length;
+    }
+
+    public int getBrickWidth() {
+        return brickWidth;
+    }
+
+    public int getBrickHeight() {
+        return brickHeight;
+    }
+}
